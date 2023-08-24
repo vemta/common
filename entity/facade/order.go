d0bf54b607336dd613afe4b516296ed0de3d2274
victorbetoni <victorbetoni@protonmail.com>
2023-08-24 09:42:11 -0300

    Using the term customer

--- entity/facade/order.go
+++ entity/facade/order.go
@@ -1,16 +1,17 @@
 package facade
 
 type DB_Order struct {
-	ID                 string  `db:"id"`
-	Customer           string  `db:"customer"`
-	Price              float64 `db:"price"`
-	PaymentMethod      string  `db:"payment_method"`
-	DiscountRaw        float64 `db:"discount_raw"`
+	ID                 string  `db:"Id"`
+	Customer           string  `db:"Customer"`
+	Price              float64 `db:"Price"`
+	PaymentMethod      string  `db:"Payment_method"`
+	DiscountRaw        float64 `db:"DiscountRaw"`
 	DiscountPercentual float64 `db:"discount_percentual"`
-	Status             int64   `db:"status"`
+	Status             int64   `db:"Status"`
 }
 
 type DB_OrderDetail struct {
-	Order string `db:"order"`
-	Item  string `db:"item"`
+	Order    string `db:"Order"`
+	Item     string `db:"Item"`
+	Quantity int    `db:"Quantity"`
 }
--- entity/order.go
+++ entity/order.go
@@ -7,10 +7,15 @@ import (
 type Order struct {
 	ID                 string           `json:"id"`
 	Customer           *User            `json:"customer"`
-	Items              *[]Item          `json:"items"`
+	Items              *[]OrderEntry    `json:"items"`
 	Price              float64          `json:"price"`
 	PaymentMethod      string           `json:"payment_method"`
 	DiscountRaw        float64          `json:"discount_raw"`
 	DiscountPercentual float64          `json:"discount_percentual"`
 	Status             enum.OrderStatus `json:"status"`
+}
+
+type OrderEntry struct {
+	Item     *Item `json:"item"`
+	Quantity int64 `json:"quantity"`
 }
