597ff68cec3c45e0b1288f5c9c4f7d112b6adbd5
victorbetoni <victorbetoni@protonmail.com>
2023-09-06 13:06:51 -0300

    removing discount fields from orders and items

--- entity/item.go
+++ entity/item.go
@@ -14,21 +14,17 @@ type Item struct {
 }
 
 type ItemValuation struct {
-	Item               string             `json:"item"`
-	DiscountRaw        float64            `json:"discount_raw"`
-	DiscountPercentual float64            `json:"discount_percentual"`
-	LastCost           float64            `json:"last_cost"`
-	LastPrice          float64            `json:"last_price"`
-	UpdatedAt          time.Time          `json:"updated_at"`
-	PriceHistory       []ItemValuationLog `json:"price_history"`
-	CostHistory        []ItemValuationLog `json:"cost_history"`
+	Item         string             `json:"item"`
+	LastCost     float64            `json:"last_cost"`
+	LastPrice    float64            `json:"last_price"`
+	UpdatedAt    time.Time          `json:"updated_at"`
+	PriceHistory []ItemValuationLog `json:"price_history"`
+	CostHistory  []ItemValuationLog `json:"cost_history"`
 }
 
 type ItemValuationLog struct {
-	Value              float64   `json:"price"`
-	DiscountRaw        float64   `json:"discount_raw"`
-	DiscountPercentual float64   `json:"discount_percentual"`
-	UpdatedAt          time.Time `json:"updated_at"`
+	Value     float64   `json:"price"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type Tax struct {
--- entity/order.go
+++ entity/order.go
@@ -5,14 +5,12 @@ import (
 )
 
 type Order struct {
-	ID                 string                  `json:"id"`
-	Customer           *Customer               `json:"customer"`
-	Items              *[]OrderEntry           `json:"items"`
-	Price              float64                 `json:"price"`
-	PaymentMethod      int                     `json:"payment_method"`
-	DiscountRaw        float64                 `json:"discount_raw"`
-	DiscountPercentual float64                 `json:"discount_percentual"`
-	Status             orderstatus.OrderStatus `json:"status"`
+	ID            string                  `json:"id"`
+	Customer      *Customer               `json:"customer"`
+	Items         *[]OrderEntry           `json:"items"`
+	Price         float64                 `json:"price"`
+	PaymentMethod int                     `json:"payment_method"`
+	Status        orderstatus.OrderStatus `json:"status"`
 }
 
 type OrderEntry struct {
